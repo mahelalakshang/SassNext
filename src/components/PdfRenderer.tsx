aed23bcb6c07737d775b966e364ac202c1d44188
Mahela Lakshan <mahela@mediio.net>
2024-02-12 14:12:07 +0530

    pdf render

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,4 +1,15 @@
-const PdfRenderer = () => {
+"use client";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer = ({ url }: PdfRendererProps) => {
+  console.log("UUUUUUUUUUUU", url);
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -17,6 +28,13 @@ const PdfRenderer = () => {
             aria-label='previous page'>
             <ChevronDown className='h-4 w-4' />
           </Button> */}
+        </div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className="max-h-full">
+            <Page pageNumber={1} />
+          </Document>
         </div>
       </div>
     </div>
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -4,13 +4,33 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
 import Dropzone from "react-dropzone";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import { Progress } from "./ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
 
 const UploadDropzone = () => {
-  const [isUploading, setIsUploading] = useState<boolean>(true);
+  const router = useRouter();
+
+  const [isUploading, setIsUploading] = useState<boolean>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
-  // const { toast } = useToast()
+
+  //   const { startUpload } = useUploadThing(
+  //   isSubscribed ? 'proPlanUploader' : 'freePlanUploader'
+  // )
+  const { startUpload } = useUploadThing("pdfUploader");
+
+  const { toast } = useToast();
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
 
   const startSimulatedProgress = () => {
     setUploadProgress(0);
@@ -31,11 +51,40 @@ const UploadDropzone = () => {
   return (
     <Dropzone
       multiple={false}
-      onDrop={(acceptedFile) => {
+      onDrop={async (acceptedFile) => {
         setIsUploading(true);
         const progressInterval = startSimulatedProgress();
+
+        const res = await startUpload(acceptedFile);
+
+        if (!res) {
+          return toast({
+            title: "Something went wrong",
+            description: "Please try again later",
+            variant: "destructive",
+          });
+        }
+
+        const [fileResponse] = res;
+
+        const key = fileResponse?.key;
+
+        if (!key) {
+          return toast({
+            title: "Something went wrong",
+            description: "Please try again later",
+            variant: "destructive",
+          });
+        }
+
+        //         clearInterval(progressInterval)
+        //         setUploadProgress(100)
+
+        //         startPolling({ key })
+
         clearInterval(progressInterval);
         setUploadProgress(100);
+        startPolling({ key });
       }}
     >
       {({ getRootProps, getInputProps, acceptedFiles }) => (
@@ -75,27 +124,26 @@ const UploadDropzone = () => {
                 <div className="w-full mt-4 max-w-xs mx-auto">
                   <Progress
                     value={uploadProgress}
-                    // indicatorColor={
-                    //   uploadProgress === 100 ? "bg-green-500" : ""
-                    // }
-                    // value={uploadProgress}
+                    indicatorColor={
+                      uploadProgress === 100 ? "bg-green-500" : ""
+                    }
                     className="h-1 w-full bg-zinc-200"
                   />
-                  {/* {uploadProgress === 100 ? (
-                    <div className='flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2'>
-                      <Loader2 className='h-3 w-3 animate-spin' />
+                  {uploadProgress === 100 ? (
+                    <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                      <Loader2 className="h-3 w-3 animate-spin" />
                       Redirecting...
                     </div>
-                  ) : null} */}
+                  ) : null}
                 </div>
               ) : null}
 
-              {/* <input
+              <input
                 {...getInputProps()}
-                type='file'
-                id='dropzone-file'
-                className='hidden'
-              /> */}
+                type="file"
+                id="dropzone-file"
+                className="hidden"
+              />
             </label>
           </div>
         </div>
