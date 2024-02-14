adbb8263c821af79dd9bcbda5eb55ce0c2de4fc1
Mahela Lakshan <mahela@mediio.net>
2024-02-14 11:19:10 +0530

    chat finished

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -12,7 +12,8 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          // url: "http://localhost:3000/api/trpc",
+          url: "https://sass-next-navy.vercel.app/api/trpc",
         }),
       ],
     })
